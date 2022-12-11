a0914b864df340006cb4da97503202361bd3b12b
KeoFoxy <arasdiel68@gmail.com>
2022-12-11 20:30:49 +0300

    Minecraft data package added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const mineflayer = require('mineflayer')
+const { pathfinder } = require('mineflayer-pathfinder')
 
-const bot = mineflayer.createBot({
+const bot = mineflayer.createBot({                                          //Bot init
     host: 'localhost',
     port: 50988,
     username: 'Miner'
 })
+
+bot.loadPlugin(pathfinder)                                                  //Add pathfinder plugin to Bot
+
+
+
 function lookAtNearestPlayer(){
     const playerFilter = (entity) => entity.type === 'player'
     const playerEntity = bot.nearestEntity(playerFilter)
@@ -15,4 +21,17 @@ function lookAtNearestPlayer(){
     bot.lookAt(pos)
 }
 
+function followPlayer(){
+    const playerCI = bot.players['KeoFoxy']
+
+    if(!playerCI){
+        bot.chat('I cant see CI')
+        return
+    }
+
+    const mcData = require('minecraft-data')(bot.version)
+}
+
 bot.on('physicTick', lookAtNearestPlayer)
+
+bot.once('spawn', followPlayer)
